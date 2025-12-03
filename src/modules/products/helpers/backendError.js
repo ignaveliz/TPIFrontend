346d08339b65a86ecf7837f5f1a8c51943901819
ignaveliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2025-12-03 14:17:40 -0300

    nuevos mensajes de error desde el backend

--- src/modules/products/helpers/backendError.js
+++ src/modules/products/helpers/backendError.js
@@ -1,5 +1,9 @@
 const frontendErrorMessage = {
   3000: 'Ya existe un producto con ese SKU',
+  3001: 'El sku del producto es obligatorio',
+  3002: 'El nombre del producto es obligatorio',
+  3003: 'El precio unitario del producto debe ser mayor a 0',
+  3004: 'La cantidad en stock del producto no puede ser negativa',
 };
 
 export {
