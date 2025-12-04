1b3c7f8eb51256681cd49beda06fe1ffca5713f5
ignaveliz <ignaciomartin.veliz@alu.frt.utn.edu.ar>
2025-12-04 11:06:51 -0300

    mensaje de error en register

--- src/modules/auth/helpers/backendError.js
+++ src/modules/auth/helpers/backendError.js
@@ -1,5 +1,6 @@
 const frontendErrorMessage = {
   1000: 'Usuario y/o Contraseña no son correctos',
+  1001: 'El nombre de usuario es requerido',
   1002: 'Usuario y/o Contraseña no son correctos',
 };
 
